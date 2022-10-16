daca0585bb4f8d5d5bd66b02feb363bd8aaca3f1
mgs28-mh <gsaputra880@gmail.com>
2022-10-16 20:29:14 +0700

    ulysse-common: set some prop
    Change-Id: I5f4079ef7d701eb27028aff494f3f8ef57bf3ed7

--- rootdir/bin/init.xiaomi.device.sh
+++ rootdir/bin/init.xiaomi.device.sh
@@ -3,9 +3,17 @@
 case "$(cat /sys/firmware/devicetree/base/model)" in
 	"Qualcomm Technologies, Inc. MSM8917-PMI8937 MTP")
 		setprop ro.vendor.xiaomi.device ugglite
+                setprop ro.vendor.fingerprint.supported 0
 		;;
 	"Qualcomm Technologies, Inc. MSM8940-PMI8937 MTP")
 		setprop ro.vendor.xiaomi.device ugg
+		# Camera
+		setprop persist.s5k3p8sp.flash.low 320
+		setprop persist.s5k3p8sp.flash.light 300
+		setprop persist.ov16885.flash.low 290
+		setprop persist.ov16885.flash.light 275
+		# Fingerprint
+		setprop ro.vendor.fingerprint.supported 1
 		;;
 esac
 
